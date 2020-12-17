860ff3ce6340e3e8f3f3c56336e72b29b3893d3c
Tobias <masterarbeithhz@gmail.com>
2020-12-17 13:54:28 +0100

    first

---
+++ Jenkinsfile
@@ -0,0 +1,59 @@
+pipeline {
+
+  environment {
+    registryCredential = 'dockerhub'
+    imagefolder = "masterarbeithhz/usecasepaxcounter:"
+    imagetag = "processing${env.BUILD_ID}"
+    giturl = 'https://github.com/masterarbeithhz/UseCaseArchitecture_Processing.git'
+  }
+  
+  agent any
+
+  stages {
+
+    stage('Checkout Source') {
+      steps {
+        git url:"${giturl}", branch:'main'
+      }
+    }
+    
+      stage("Build image") {
+            steps {
+                script {
+                    myapp = docker.build("${imagefolder}${imagetag}")
+                }
+            }
+        }
+    
+      stage("Push image") {
+            steps {
+                script {
+                    docker.withRegistry('https://registry.hub.docker.com', registryCredential) {
+                            myapp.push("${imagetag}")
+                    }
+                }
+            }
+        }
+
+      stage("Prepare Yaml") {
+        steps {
+          script {
+            def data = readFile file: "kubmanifest.yaml"
+            data = data.replaceAll("JSVAR_DOCKERIMAGE", "${imagefolder}${imagetag}")
+            echo data
+            writeFile file: "kubmanifest.yaml", text: data
+          }
+        }
+      }
+    
+    stage('Deploy App') {
+      steps {
+        script {
+          kubernetesDeploy(configs: "kubmanifest.yaml", kubeconfigId: "mykubeconfig1")
+        }
+      }
+    }
+
+  }
+
+}
---
+++ image_files/json.php
@@ -0,0 +1,56 @@
+<html>
+ <head>
+  <title>1PHP-Test</title>
+ </head>
+ <body>
+<?php
+
+//Make sure that it is a POST request.
+if(strcasecmp($_SERVER['REQUEST_METHOD'], 'POST') != 0){
+    throw new Exception('Request method must be POST!');
+}
+ 
+//Make sure that the content type of the POST request has been set to application/json
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+if(strcasecmp($contentType, 'application/json') != 0){
+    throw new Exception('Content type must be: application/json');
+}
+
+// Read the input stream
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+$hardware_serial = $obj->hardware_serial;
+$wifi = $obj->payload_fields->wifi;
+$ble = $obj->payload_fields->ble;
+
+
+  $conn = mysqli_connect("mysql-service","root","Philipp1","testdb");
+
+  if ($conn->connect_error) {
+    die("ERROR: Unable to connect: " . $conn->connect_error);
+  }
+
+
+
+$sqlQuery_devices = "SELECT dev_id from devices WHERE dev_Id='$hardware_serial'";
+$result_devices = mysqli_query($conn,$sqlQuery_devices);
+$data_sensors = array();
+foreach ($result_devices as $row) {
+        $data_sensors[] = $row;
+}
+
+// checks if sensor id is listed in your devices table if not massage is ignored
+if ($data_sensors[0][dev_id]) {
+  $stmt = $conn->prepare("INSERT INTO readings (dev_id, count_wifi, count_ble) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $hardware_serial, $wifi, $ble);
+  $stmt->execute();
+  echo 'sensor value saved successfully';
+} else {
+    echo 'the used sensor dev_id is not known';
+}
+
+  $stmt->close();
+  $conn->close();
+?>
+ </body>
+</html>
